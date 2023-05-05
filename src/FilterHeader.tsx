48b0f4a9440ed8c400c504188f608176fa467d6b
Sadeq Morshedloo <sadeq.morshedloo@campus-support.ca>
2023-05-05 22:42:47 +0000

    feat: injects multiple filters

--- src/FilterHeader.tsx
+++ src/FilterHeader.tsx
@@ -2,8 +2,8 @@ import { Config, ConfigKey, useConfig } from "./store/useConfig";
 
 function FilterHeader(props: {
   config?: Config;
-  isActive: boolean;
-  onEdit: (configKey?: ConfigKey) => void;
+  active: boolean;
+  onEdit?: (configKey?: ConfigKey) => void;
 }) {
   const { removeConfig, updateConfig } = useConfig((state) => ({
     removeConfig: state.removeConfig,
@@ -21,43 +21,44 @@ function FilterHeader(props: {
   };
   return (
     <div
-      className={`flex items-center  justify-start gap-2 btn  w-full ${
-        props.isActive ? "btn-accent" : "btn-primary"
-      }`}
+      className={`btn w-full h-fit ${
+        props.active ? "btn-accent" : "btn-primary"
+      } pb-2`}
     >
-      <input
-        type="checkbox"
-        className={`form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out ${
-          props.config ? "visible" : "invisible"
-        }`}
-        checked={props.config?.selected}
-        onChange={handleToggleConfig}
-      />
-      {props.config ? (
-        <p>
-          {props.config?.method}-{props.config?.url}
-        </p>
-      ) : (
-        <p>New Filter</p>
-      )}
-      {props.config && (
-        <div className="ml-auto flex">
-          <button
-            className="btn btn-sm btn-primary ml-2"
-            onClick={() => {
-              props.onEdit(props.config?.key);
-            }}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-sm btn-error ml-2"
-            onClick={handleRemoveConfig}
-          >
-            Delete
-          </button>
-        </div>
-      )}
+      <div className={`w-full grid grid-cols-[1fr_5fr_2fr] items-center gap-2`}>
+        <input
+          type="checkbox"
+          className={`form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out ${
+            props.config ? "visible" : "invisible"
+          }`}
+          checked={props.config?.selected}
+          onChange={handleToggleConfig}
+        />
+        {props.config ? (
+          <h3 className="text-left truncate">{props.config?.method}</h3>
+        ) : (
+          <p>New Filter</p>
+        )}
+        {props.config && (
+          <div className="ml-auto flex gap-2">
+            <button
+              className="btn btn-xs btn-ghost px-0"
+              onClick={() => {
+                props.onEdit?.(props.config?.key);
+              }}
+            >
+              <span>{`📝`}</span>
+            </button>
+            <button
+              className="btn btn-xs btn-ghost px-0"
+              onClick={handleRemoveConfig}
+            >
+              <span>{`❌`}</span>
+            </button>
+          </div>
+        )}
+      </div>
+      <p className="w-full text-left text-xs break-all">{props.config?.url}</p>
     </div>
   );
 }
